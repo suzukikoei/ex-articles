25b248b72cd44b2dae61aa990d8b30358f41604b
suzukikoei <koeich6@gmail.com>
2025-05-27 15:26:42 +0900

    コメント、記事ドメインの新規作成

---
+++ src/main/java/com/example/domain/Article.java
@@ -0,0 +1,59 @@
+package com.example.domain;
+
+import java.util.List;
+
+/**
+ * 記事ドメイン.
+ */
+public class Article {
+    /** 記事ID */
+    private Integer id;
+    /** 名前　*/
+    private String name;
+    /** 記事内容*/
+    private String content;
+    /** コメント　*/
+    private List<Comment> comments;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", content='" + content + '\'' +
+                ", comments=" + comments +
+                '}';
+    }
+}
