cd57eaa61481ed919cd6f75a3b1a651ef9d34af0
suzukikoei <koeich6@gmail.com>
2025-05-27 13:55:41 +0900

    first commit

---
+++ src/main/java/com/example/ExArticlesApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExArticlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExArticlesApplication.class, args);
+	}
+
+}
